67e73dbb10e47520aadf5804b057402440f7cfcf
Omar Amin <Omar_amin@live.com>
2015-09-04 20:29:05 +0200

    Adding food to the snake world

--- Snake/Snake/Main.cpp
+++ Snake/Snake/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <conio.h>
+#include <math.h>
 
 using namespace std;
 
@@ -26,9 +27,19 @@ struct Snake
 	int size;
 };
 
-struct WorldOfSnake
+struct Food
+{
+	int x;
+	int y;
+	int timelefttodisappear;
+};
+
+struct World
 {
 	Snake snake;
+	Food food[5];
+	int foodcount;
+	int score;
 	bool gameover;
 	char world[dim][dim];
 };
@@ -39,14 +50,14 @@ void InitializeSnake(Snake & snk);
 
 void SnakeDraw();
 
-void InitializeWorldOfSnake(WorldOfSnake & wofsnk);
+void InitializeWorldOfSnake(World & wofsnk);
 
-void AddSnakeToTheWorld(WorldOfSnake & wofsnk);
+void AddSnakeToTheWorld(World & wofsnk);
 
-
+bool GeneratedOnSnake(Snake sn, int x, int y);
 void InitializeWorld(char world[dim][dim]);
 
-void DrawWorld(WorldOfSnake w);
+void DrawWorld(World w);
 
 void MoveSnakeToTheRight(Snake & snake);
 
@@ -55,6 +66,9 @@ bool MoveSnake(Snake & snake,Direction direction);
 void MoveNode(Node & node, Direction dir);
 
 bool AreThereAnyCollisions(Snake snake);
+
+void AddFoodToTheWorld(World & w);
+
 
 Direction GetDirection(char choice)
 {
@@ -71,11 +85,16 @@ Direction GetDirection(char choice)
 }
 
 void MoveNodeNormally(Node & node);
+
+void InitializeFood(World &w);
+
+bool SnakeEatFood(World &w);
 int main()
 {
-	WorldOfSnake w;
+	World w;
 	char choice = 's';
 	InitializeSnake(w.snake);
+	InitializeFood(w);
 	bool gameover = false;
 	while (!gameover)
 	{
@@ -90,9 +109,36 @@ int main()
 			cout << endl << "Not Valid Direction, Please Enter a,s,d,w only" << endl;
 		}
 		gameover = MoveSnake(w.snake,GetDirection(choice));
+		if (SnakeEatFood(w))
+			w.snake.size++;
 	}
 	system("pause");
 	return 0;
+}
+
+bool SnakeEatFood(World &w)
+{
+	int headx = w.snake.nodes[0].x;
+	int heady = w.snake.nodes[0].y;
+	for (int i = 0; i < w.foodcount; i++)
+	{
+		if (headx == w.food[i].x
+			&& heady == w.food[i].y)
+		{
+			
+			w.food[i].timelefttodisappear = 1;
+			w.score += 100;
+			return true;
+		}
+	}
+	return false;
+}
+
+
+void InitializeFood(World & w)
+{
+	w.foodcount = 0;
+	w.score = 0;
 }
 
 void MoveNode(Node & node, Direction dir)
@@ -140,7 +186,7 @@ bool MoveSnake(Snake & snake, Direction direction)
 	Direction HeadDirection = snake.nodes[0].dir;
 	bool ShouldMoveNormally = IsMovingInItsDirection(direction, HeadDirection);
 	int snakesize = snake.size;
-	for (int i = snakesize-1;i > 0 ;i--)
+	for (int i = snakesize;i > 0 ;i--)
 	{
 		// snake was     : ***>
 		// snake will be :  ***>
@@ -159,9 +205,9 @@ bool MoveSnake(Snake & snake, Direction direction)
 		MoveNode(snake.nodes[0],direction);
 	}
 
-
 	return AreThereAnyCollisions(snake);
 }
+
 bool AreThereAnyCollisions(Snake snake)
 {
 	// collision happens only when the head hits anyother node
@@ -190,14 +236,44 @@ void MoveSnakeToTheRight(Snake & snake)
 	}
 }
 
-
-void InitializeWorldOfSnake(WorldOfSnake & wofsnk)
+void AdjustFoodTime(World &w)
 {
-	InitializeWorld(wofsnk.world);
-	AddSnakeToTheWorld(wofsnk);
+	// just simpler name
+	int count = w.foodcount;
+
+
+	for (int i = 0; i < count; i++)
+	{
+		w.food[i].timelefttodisappear--;
+		if (w.food[i].timelefttodisappear == 0)
+		{
+			// remove it
+			for (int j = i;j < count;j++)
+			{
+				// move the rest backword one step starting from i to the end;
+				w.food[j].x = w.food[j + 1].x;
+				w.food[j].y = w.food[j + 1].y;
+				w.food[j].timelefttodisappear = w.food[j + 1].timelefttodisappear;
+			}
+			// you've deleted one item, so update the counters
+			count--;
+			// go back one food item as afte the iteration i++ will be executed and you don't want
+			// to skip any items
+			i--;
+		}
+	}
+	w.foodcount = count;
 }
 
-void AddSnakeToTheWorld(WorldOfSnake & wofsnk)
+void InitializeWorldOfSnake(World & wofsnk)
+{
+	InitializeWorld(wofsnk.world);
+	AdjustFoodTime(wofsnk);
+	AddSnakeToTheWorld(wofsnk);
+	AddFoodToTheWorld(wofsnk);
+}
+
+void AddSnakeToTheWorld(World & wofsnk)
 {
 	int size = wofsnk.snake.size;
 	for (int i = 0; i < size; i++)
@@ -208,7 +284,7 @@ void AddSnakeToTheWorld(WorldOfSnake & wofsnk)
 	}
 }
 
-void DrawWorld(WorldOfSnake w)
+void DrawWorld(World w)
 {
 	for (size_t i = 0; i < dim; i++)
 	{
@@ -218,7 +294,10 @@ void DrawWorld(WorldOfSnake w)
 		}
 		cout << endl;
 	}
+	cout << endl;
+	cout << "\t\t\t Score : " << w.score;
 }
+
 
 void InitializeWorld(char world[dim][dim])
 {
@@ -270,5 +349,56 @@ void SnakeDraw()
 				cout << " ";
 		}
 		cout << endl;
+	}
+}
+
+bool GeneratedOnSnake(Snake sn, int x, int y)
+{
+	for (size_t i = 0; i < sn.size; i++)
+	{
+		if (sn.nodes[i].x == x
+			&& sn.nodes[i].y == y)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void AddFoodToTheWorld(World &w)
+{
+	// Do we need to generate food ?
+	int foodcount = w.foodcount;
+	if (foodcount == 4)
+		return;
+	bool Generate = ((4 - foodcount) * (rand() % 10)) > 26; 
+	if(Generate)
+	{
+		// rand() % (dim - 2) + 1   ==== random number between 1 and dim-1 (inside the world)
+		int x = rand() % (dim - 2) + 1;
+		int y = rand() % (dim - 2) + 1;
+		// check if the generated food isn't part of the snake
+		while (GeneratedOnSnake(w.snake, x, y))
+		{
+			x = rand() % (dim - 2) + 1;
+			y = rand() % (dim - 2) + 1;
+		}
+		int headx = w.snake.nodes[0].x; // getting head x position
+		int heady = w.snake.nodes[0].y; // getting head y position
+		int distance = abs(headx-x) + abs(heady-y);
+		
+		int timetodisappear = distance + 1 + rand() % 5;  // rand()%5 to give space for any mistake on the way
+		// accumelate on other food objects
+		w.food[foodcount].x = x;
+		w.food[foodcount].y = y;
+		w.food[foodcount].timelefttodisappear = timetodisappear;
+		w.foodcount++;
+	}
+
+	for (size_t i = 0; i < w.foodcount; i++)
+	{
+		int x = w.food[i].x;
+		int y = w.food[i].y;
+		w.world[x][y] = '$';
 	}
 }
